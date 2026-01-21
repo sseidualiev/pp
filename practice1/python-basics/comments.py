d2ed6d330aa7ce0328d03de865f029fe89e1b99f
sseidualiev <seidualiev01@gmail.com>
2026-01-21 21:17:44 +0500

    using multiline comments

--- practice1/python-basics/comments.py
+++ practice1/python-basics/comments.py
@@ -3,3 +3,7 @@ print("Hello, KBTU!")
 print("Hello, KBTU!") #This is a comment too
 #print("Hello, World!") can be used to prevent executing code
 print("Cheers, Mate!")
+#This is a comment
+#written in
+#more than just one line
+print("Hello, KBTU!")
