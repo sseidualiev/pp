e3dd9016e2fee4f4080aab5addfd421b787cf32c
sseidualiev <seidualiev01@gmail.com>
2026-01-23 18:30:15 +0500

    slicing strings by index

--- practice1/python-basics/strings.py
+++ practice1/python-basics/strings.py
@@ -42,3 +42,6 @@ print("expensive" not in txt)
 txt = "The best things in life are free!"
 if "expensive" not in txt:
   print("No, 'expensive' is NOT present.")
+
+b = "Hello, World!"
+print(b[2:5])
