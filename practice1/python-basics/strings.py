0ebc3fc6e14fff8b9f4a8b39137d1edcc3f661d0
sseidualiev <seidualiev01@gmail.com>
2026-01-23 18:19:41 +0500

    or using three single quotes

--- practice1/python-basics/strings.py
+++ practice1/python-basics/strings.py
@@ -13,3 +13,9 @@ consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
 print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
