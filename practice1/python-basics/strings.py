897ae830ab024de806be4c3ac283d1b7e9361a7c
sseidualiev <seidualiev01@gmail.com>
2026-01-23 18:21:50 +0500

    check if not in the string

--- practice1/python-basics/strings.py
+++ practice1/python-basics/strings.py
@@ -35,3 +35,6 @@ print("free" in txt)
 txt = "The best things in life are free!"
 if "free" in txt:
   print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
