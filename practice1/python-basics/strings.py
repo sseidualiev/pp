8ef0cce5b0ec477cadc58fb459e228e70dd3d3a9
sseidualiev <seidualiev01@gmail.com>
2026-01-23 18:19:19 +0500

    assigning multiline string value by using triple quotes

--- practice1/python-basics/strings.py
+++ practice1/python-basics/strings.py
@@ -7,3 +7,9 @@ print('He is called "Johnny"')
 
 a = "Hello"
 print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
