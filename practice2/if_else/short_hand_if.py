ef33dba9338ffb7742bfda735c58c73624d675ad
sseidualiev <seidualiev01@gmail.com>
2026-01-28 11:14:37 +0500

    using shorthand if in practice

--- practice2/if_else/short_hand_if.py
+++ practice2/if_else/short_hand_if.py
@@ -14,3 +14,12 @@ print("Bigger is", bigger)
 a = 330
 b = 330
 print("A") if a > b else print("=") if a == b else print("B")
+
+x = 15
+y = 20
+max_value = x if x > y else y
+print("Maximum value:", max_value)
+
+username = ""
+display_name = username if username else "Guest"
+print("Welcome,", display_name)
